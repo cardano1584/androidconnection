807f0863eb9856cb1001c63b24c3ddfb1ce2d378
David <avina.david.s@gmail.com>
2023-10-20 17:43:06 -0700

    Changing the language

--- tts_script.py
+++ tts_script.py
@@ -6,10 +6,12 @@ import os
 text_to_speech = "Hello, this is a text-to-speech demonstration in Termux."
 
 # Create a gTTS object
-tts = gTTS(text_to_speech, lang='en')
+tts = gTTS(text_to_speech, lang='en-GB')
 
 # Save the speech as an audio file (e.g., tts_output.mp3)
 tts.save("tts_output.mp3")
 
 # Play the audio using Termux's built-in media player (mpv)
 os.system("termux-media-player play tts_output.mp3")
+
+# python tts_script.py
